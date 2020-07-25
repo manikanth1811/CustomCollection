80b763b2e546c59dbe5dd0d49064ac5737f1c4ba
manikanth18 <manikanthpolice18@gmail.com>
2020-07-25 10:45:43 +0530

    Assignment complete

---
+++ CustomCollections/src/test/java/ManiEpam6/Mani_CustomCollections/AppTest.java
@@ -0,0 +1,20 @@
+package ManiEpam6.Mani_CustomCollections;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
